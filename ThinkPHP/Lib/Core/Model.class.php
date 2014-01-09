d6817898f71014499061e088c75dcd7b215d715c
pakey <pakey@qq.com>
2014-01-09 19:50:47 +0800

    修改之前用F保存缓存的地方为cache函数

--- ThinkPHP/Lib/Core/Model.class.php
+++ ThinkPHP/Lib/Core/Model.class.php
@@ -110,7 +110,7 @@ class Model {
             // 如果数据表字段没有定义则自动获取
             if(C('DB_FIELDS_CACHE')) {
                 $db   =  $this->dbName?$this->dbName:C('DB_NAME');
-                $fields = F('_fields/'.strtolower($db.'.'.$this->name));
+                $fields = cache('_fields/'.strtolower($db.'.'.$this->name));
                 if($fields) {
                     $version    =   C('DB_FIELD_VERSION');
                     if(empty($version) || $fields['_version']== $version) {
@@ -154,7 +154,7 @@ class Model {
         if(C('DB_FIELDS_CACHE')){
             // 永久缓存数据表信息
             $db   =  $this->dbName?$this->dbName:C('DB_NAME');
-            F('_fields/'.strtolower($db.'.'.$this->name),$this->fields);
+            cache('_fields/'.strtolower($db.'.'.$this->name),$this->fields);
         }
     }
 
